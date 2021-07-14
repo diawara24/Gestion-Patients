87a7faf7f6a0138c7d781336515496704f30b435
Mourtalla faty Diawaa <49997817+diawara24@users.noreply.github.com>
2021-07-14 16:50:49 +0200

    commit for admin authentification

--- src/main/java/org/sid/springmvc/security/SecurityConfig.java
+++ src/main/java/org/sid/springmvc/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,23 +18,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        // {noop} permet de desactiver la verification du mot de pass encoder
-        /*auth.inMemoryAuthentication().withUser("user1").password("{noop}passer").roles("USER");
-        auth.inMemoryAuthentication().withUser("user2").password("{noop}passer").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("{noop}passer").roles("USER","ADMIN");*/
-
-        // Connection via le JDBC
         PasswordEncoder passwordEncoder = passwordEncoder();
-        System.out.println("***********************************");
+        System.out.println("*************************************************");
         System.out.println(passwordEncoder.encode("passer"));
-        System.out.println("***********************************");
+        System.out.println("*************************************************");
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .usersByUsernameQuery("SELECT username as principal, password as credentials, active FROM users WHERE username=?")
                 .authoritiesByUsernameQuery("SELECT username as principal, role as role FROM users_roles WHERE username=?")
                 .passwordEncoder(passwordEncoder)
                 .rolePrefix("ROLE_");
+
     }
 
     private PasswordEncoder passwordEncoder() {
@@ -43,24 +38,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     //chercher le user dans la base de donnees
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // specifier la page de connection
+        http.formLogin().loginPage("/login");
         /*Authentification basic basé sur le protocole http
         http.httpBasic();*/
-        // spevifier la page de connection
-         http.formLogin().loginPage("/connect");
-        /*toute requete http necessite une authentification
-        http.authorizeRequests().anyRequest().authenticated();*/
         http.authorizeRequests().antMatchers("/admin**/**","/ajout**/**","/save**/**","/delete**/**","/edit**/**","/update**/**","/form**/**").hasRole("ADMIN");
-        http.authorizeRequests().antMatchers("/user**/**").permitAll();
-
+        http.authorizeRequests().antMatchers("/user**/**","/login**/**").permitAll();
+        //toute requete http necessite une authentification
         http.authorizeRequests().anyRequest().authenticated();
-        // Active la protection contre les attaques de type csrf
-        http.csrf();
         // Personaliser la page d'erreur
         http.exceptionHandling().accessDeniedPage("/errorNotAuthorized");
+        // http.csrf();
     }
 
-
-
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
 
     /*protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
@@ -80,6 +76,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authoritiesByUsernameQuery("SELECT username as principal, role as role FROM users_roles WHERE username=?")
                 .passwordEncoder(passwordEncoder)
                 .rolePrefix("ROLE_");
+    }*/
+
+    /*@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // {noop} permet de desactiver la verification du mot de pass encoder
+        *//*auth.inMemoryAuthentication().withUser("user1").password("{noop}passer").roles("USER");
+        auth.inMemoryAuthentication().withUser("user2").password("{noop}passer").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}passer").roles("USER","ADMIN");*//*
+
+        // Connection via le JDBC
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        System.out.println("*************************************************");
+        System.out.println(passwordEncoder.encode("passer"));
+        System.out.println("*************************************************");
+        *//*auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT username as principal, password as credentials, active FROM users WHERE username=?")
+                .authoritiesByUsernameQuery("SELECT username as principal, role as role FROM users_roles WHERE username=?")
+                .passwordEncoder(passwordEncoder)
+                .rolePrefix("ROLE_");*//*
     }*/
 
 }
--- src/main/java/org/sid/springmvc/security/SecurityController.java
+++ src/main/java/org/sid/springmvc/security/SecurityController.java
@@ -3,16 +3,28 @@ package org.sid.springmvc.security;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class SecurityController {
+
+    @GetMapping(path = "/login")
+    public String loginPage(){
+        return "login";
+    }
+
+    @GetMapping(path = "/logout")
+        public String logout(HttpServletRequest request) throws ServletException {
+            request.logout();
+            return "redirect:/login";
+        }
+
 
     @GetMapping(path = "/errorNotAuthorized")
     public String error(){
         return "403";
     }
 
-    @GetMapping(path = "/connect")
-    public String login(){
-        return "login";
-    }
+
 }
